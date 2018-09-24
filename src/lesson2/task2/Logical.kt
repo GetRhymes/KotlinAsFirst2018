240b2e6260e5c628afaccdee7afd12f79dc59d3c
GetRhymes <tarasenko.ns@edu.spbstu.ru>
2018-09-25 00:09:07 +0300

    Les2 was finished

--- src/lesson2/task2/Logical.kt
+++ src/lesson2/task2/Logical.kt
@@ -4,6 +4,7 @@ package lesson2.task2
 
 import lesson1.task1.sqr
 import kotlin.math.abs
+import kotlin.math.sqrt
 
 /**
  * Пример
@@ -57,7 +58,7 @@ fun daysInMonth(month: Int, year: Int): Int = when {
  * Вернуть true, если утверждение верно
  */
 fun circleInside(x1: Double, y1: Double, r1: Double,
-                 x2: Double, y2: Double, r2: Double): Boolean = sqr(x1 - x2) + sqr(y1 - y2) + r1 <= r2
+                 x2: Double, y2: Double, r2: Double): Boolean = sqrt(sqr(x1 - x2) + sqr(y1 - y2)) + r1 <= r2
 
 /**
  * Средняя
