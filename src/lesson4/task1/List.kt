9a5d64c5d79038338820401f03cfda0cd8879d50
GetRhymes <tarasenko.ns@edu.spbstu.ru>
2018-10-16 22:42:22 +0300

    check 4les

--- src/lesson4/task1/List.kt
+++ src/lesson4/task1/List.kt
@@ -244,10 +244,11 @@ fun convert(n: Int, base: Int): List<Int> {
  * строчными буквами: 10 -> a, 11 -> b, 12 -> c и так далее.
  * Например: n = 100, base = 4 -> 1210, n = 250, base = 14 -> 13c
  */
-fun convertToString(n: Int, base: Int): String = convert(n, base).joinToString(
-        separator = "",
-        transform = { if (it > 9) ('a' + it - 10).toString() else "$it" }
-)
+fun convertToString(n: Int, base: Int): String = TODO()
+// convert(n, base).joinToString(
+// separator = "",
+// transform = { if (it > 9) ('a' + it - 10).toString() else "$it" }
+//)
 
 /**
  * Средняя
